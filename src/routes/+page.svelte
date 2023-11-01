a9d4bba956e50e74bed7acb87386eaffa1568ee1
Casimir Richardson <133361185+RendingApart@users.noreply.github.com>
2023-11-01 13:56:25 +0000

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script lang="ts">
+	import { writable } from "svelte/store";
+
+  let count = writable(0);
+</script>
+
+<button on:click={() => {
+  $count++
+}}>{$count}</button>
